6293ac24b6e55743f175ddb9a2615dea9831a032
shekaf <en.omid.davari@gmail.com>
2020-03-11 20:36:24 +0330

    create

--- app/src/main/java/com/example/mahsa/MainActivity.kt
+++ app/src/main/java/com/example/mahsa/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.mahsa
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val intro_page = page1()
+        val fm: FragmentManager = supportFragmentManager
+        val ft: FragmentTransaction = fm.beginTransaction()
+        ft.replace(R.id.intro_frame, intro_page)
+        ft.commit()
     }
 }
